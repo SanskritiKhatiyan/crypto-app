bf57cc0e01325a44255e98f2dff863d1ba144947
Arjun Singh Rajput <rajputarjun1708@protonmail.com>
2021-10-08 23:04:53 +0530

    Added fields in Signup form

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -10,9 +10,10 @@ const Signup = () => {
           <label htmlFor="chk" aria-hidden="true">
             Sign up
           </label>
-          <input type="text" name="txt" placeholder="User name" required />
+          <input type="text" name="txt" placeholder="Full name" required />
           <input type="email" name="email" placeholder="Email" required />
           <input type="password" name="pswd" placeholder="Password" required />
+          <input type="password" name="pswd" placeholder="Confirm Password" required />
           <button>Sign up</button>
         </form>
       </div>
