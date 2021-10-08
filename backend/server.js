8a99f6414a5a542db7710493b1e4b5d20c4fd3c0
Abhishek <akthakuron120@gmail.com>
2021-10-08 12:30:41 +0530

    Connecting app to the Mongoose Database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,26 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+// Connecting application to Hosted DataBase
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`DB connection successfull🔥`);
+  })
+  .catch((err) => {
+    console.log(`Error connecting in DB ${err}`);
+  });
 
 const port = process.env.PORT || 4000;
 const server = app.listen(port, (err) => {
