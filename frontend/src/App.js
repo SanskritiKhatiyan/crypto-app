8815447879f62705335b2c788a91250849d8eb7e
Abhishek <akthakuron120@gmail.com>
2021-10-08 23:03:06 +0530

    Minor Bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
-      <h1> hello</h1>
+      <Signup />
     </div>
   );
 }
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import "./Signup.css"
-import "../Assets/1.jpg"
+import React from "react";
+import "./Signup.css";
+import "../assets/1.jpg";
 
 const Signup = () => {
-    return (
-        <div className="main">
-            <div className="signup">
-          <form>
-            <label htmlFor="chk" aria-hidden="true">Sign up</label>
-            <input type="text" name="txt" placeholder="User name" required />
-            <input type="email" name="email" placeholder="Email" required />
-            <input type="password" name="pswd" placeholder="Password" required />
-            <button>Sign up</button>
-          </form>
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="main">
+      <div className="signup">
+        <form>
+          <label htmlFor="chk" aria-hidden="true">
+            Sign up
+          </label>
+          <input type="text" name="txt" placeholder="User name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="password" name="pswd" placeholder="Password" required />
+          <button>Sign up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
