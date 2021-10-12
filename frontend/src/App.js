1a4f8c82a36320c0c4294287d6a81695187dfb9f
Abhishek <akthakuron120@gmail.com>
2021-10-12 13:30:12 +0530

    Add components in seperate subfolders.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Signup from "./components/Signup";
-import About from "./components/About";
-import Home from "./components/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Signup from "./components/Signup/Signup";
+import Signin from "./components/Signin/Signin";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Mainbar from "./components/Mainbar/Mainbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Mainbar />
       <Route exact path="/">
         <Home />
       </Route>
 
       <Route path="/about">
         <About />
+      </Route>
+      <Route path="/signin">
+        <Signin />
       </Route>
       <Route path="/signup">
         <Signup />
---
+++ frontend/src/components/Mainbar/Mainbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Mainbar.css";
+
+const Mainbar = () => {
+  return (
+    <div>
+      <h1>Hello Mainbar</h1>
+    </div>
+  );
+};
+
+export default Mainbar;
