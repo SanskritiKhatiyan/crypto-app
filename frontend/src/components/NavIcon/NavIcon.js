cdd2b51b1073f1307fabc183d409f4abff27fea1
shivam <shivamsaraaswat3@gmail.com>
2021-10-14 21:49:47 +0530

    adding navbar

---
+++ frontend/src/components/NavIcon/NavIcon.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { useState } from "react";
+import "./NavIcon.css"
+
+const NavIcon =()=>
+{
+  // const[showMenu,setShowMenu]=useState(false);
+    
+    return(
+    <label id="icon">
+    {/* <
+      /* onClick={() => setShowMenu(!showMenu)}  */
+      /> */}
+  </label>
+    );
+}
+export default NavIcon;
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,54 +1,106 @@
-import React from "react";
-import "./Navbar.css";
-import "bootstrap/dist/css/bootstrap.css";
-import { NavLink } from "react-router-dom";
+// import React from "react";
+// import "./Navbar.css";
+// import "bootstrap/dist/css/bootstrap.css";
+// import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+// const Navbar = () => {
+//   return (
+//     <div>
+//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//         <NavLink class="navbar-brand" to="/">
+//           Navbar
+//         </NavLink>
+//         <button
+//           class="navbar-toggler"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#navbarSupportedContent"
+//           aria-controls="navbarSupportedContent"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span class="navbar-toggler-icon"></span>
+//         </button>
+
+//         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul class="navbar-nav mr-auto">
+//             <li class="nav-item active">
+//               <NavLink class="nav-link" to="/">
+//                 Home <span class="sr-only">(current)</span>
+//               </NavLink>
+//             </li>
+//             <li class="nav-item">
+//               <NavLink class="nav-link" to="/about">
+//                 About
+//               </NavLink>
+//             </li>
+//             {/* <li class="nav-item">
+//               <NavLink class="nav-link" to="/signin">
+//                 Login
+//               </NavLink>
+//             </li> */}
+//             <li class="nav-item">
+//               <NavLink class="nav-link" to="/signup">
+//                 Register
+//               </NavLink>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+import React from "react";
+import { useState } from "react";
+import "./Navbar.css"
+// import NavIcon from "./NavIcon";
+
+function NavBar(props) {
+  // const[click,setclick]=useState(false);
+  
+  const NavMenuItems = [
+    {
+      id: "1",
+      url: "#",
+      menu: "LOGIN",
+      cname: "nav-menu",
+    },
+
+    {
+      id: "2",
+      url: "#",
+      menu: "SIGN UP",
+      cname: "nav-menu",
+    },
+    {
+      id: "3",
+      url: "#",
+      menu: "ABOUT",
+      cname: "nav-menu",
+    },
+  ];
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink class="navbar-brand" to="/">
-          Navbar
-        </NavLink>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <NavLink class="nav-link" to="/">
-                Home <span class="sr-only">(current)</span>
-              </NavLink>
+      <nav className="nav-main">
+        <label className="logo">CRYTO CURRENCY</label>
+         
+        {/* <NavIcon/> */}
+        <ul className="nav-items">
+          {NavMenuItems.map(({ id, url, cname, menu }) => (
+            <li>
+              <a className={cname} href={url} >
+                {menu}
+              </a>
             </li>
-            <li class="nav-item">
-              <NavLink class="nav-link" to="/about">
-                About
-              </NavLink>
-            </li>
-            {/* <li class="nav-item">
-              <NavLink class="nav-link" to="/signin">
-                Login
-              </NavLink>
-            </li> */}
-            <li class="nav-item">
-              <NavLink class="nav-link" to="/signup">
-                Register
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+          ))}
+        </ul>
+        
       </nav>
     </div>
   );
-};
+}
 
-export default Navbar;
+export default NavBar;
