c61aede76340b0fd3670dc0f56a7388beb80e7af
Sanskriti Khatiyan <khatiyan.sanskriti@gmail.com>
2021-10-17 23:33:52 +0530

    Made raw news page template

--- frontend/src/components/News/News.js
+++ frontend/src/components/News/News.js
@@ -1,74 +1,25 @@
 import React from 'react';
 import "./News.css";
+import NewsItem from './NewsItem';
 
 const News = () => {
   return (
     <div>
       <div className="news-box" >
         <h1> NEWS PAGE IS LOADING.....</h1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit
-        voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit 
-        anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit
-        voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit 
-        anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit
-        voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit 
-        anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit
-        voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit 
-        anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit
-        voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit 
-        anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit
-        voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit 
-        anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit
-        voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit 
-        anim id est laborum.
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
+        <NewsItem> </NewsItem>
       </div>
     </div>
   )
---
+++ frontend/src/components/News/NewsItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Newsitem.css";
+
+const NewsItem = () => {
+    return (
+        <div className="news-item">
+            <div className="news-img">image </div>
+            <div className="news-text"> news text</div>
+        </div>
+       
+    )
+}
+
+export default NewsItem;
