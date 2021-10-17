5ce6b1f8b21636dcd747f1b658cf0886d67f9127
Sanskriti Khatiyan <khatiyan.sanskriti@gmail.com>
2021-10-18 00:08:53 +0530

    modified news template

--- frontend/src/components/News/NewsItem.js
+++ frontend/src/components/News/NewsItem.js
@@ -4,8 +4,10 @@ import "./Newsitem.css";
 const NewsItem = () => {
     return (
         <div className="news-item">
+            <div className="news-text"> news text
+            <div> source</div>
+            </div>
             <div className="news-img">image </div>
-            <div className="news-text"> news text</div>
         </div>
        
     )
