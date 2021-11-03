196ae11efba7ac31f47c7e769dba45f5a664c193
Abhishek <akthakuron120@gmail.com>
2021-11-03 16:54:20 +0530

    Created Search Bar and added Search filter functionality.

--- frontend/src/components/Coins/CoinData.js
+++ frontend/src/components/Coins/CoinData.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import App from "./CoinCard/coincard";
-import "./CoinData.css"
+import "./CoinData.css";
 
 export default function CoinData() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchFilter, setSearchFilter] = useState("");
 
   useEffect(() => {
     axios(
@@ -28,23 +29,43 @@ export default function CoinData() {
   if (error) return "Error!";
 
   return (
-    <div className="card-align">
-    
-      {data.map((coin) => {
-        return (
-          
-          <App
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            symbol={coin.symbol}
-            currentPrice={coin.current_price}
-            marketCap={coin.market_cap}
-          />
-          
-        );
-      })}
-      
+    <div className="card-container">
+      {/* SearchBar  */}
+      <div className="search-bar__search">
+        <input
+          className="search-bar__input"
+          placeholder="Search..."
+          type="text"
+          onChange={(event) => {
+            setSearchFilter(event.target.value);
+          }}
+        />
+      </div>
+      <div className="card-align">
+        {/* Filtering Data Based On Search */}
+        {data
+          .filter((val) => {
+            if (searchFilter == "") {
+              return val;
+            } else if (
+              val.name.toLowerCase().includes(searchFilter.toLocaleLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((coin) => {
+            return (
+              <App
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                currentPrice={coin.current_price}
+                marketCap={coin.market_cap}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 }
