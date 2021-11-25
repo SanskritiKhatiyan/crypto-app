652aee6c3aa060c04f7b7da1b4a4b4edd0cbec0d
Abhishek <akthakuron120@gmail.com>
2021-11-25 19:07:35 +0530

    Revert "Revert "Changes""
    This reverts commit 57a7502b7bd3679c1a230fd197bc6cbc57eeb1d0.

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,60 +1,56 @@
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//     faYoutube,
+//     faFacebook,
+//     faTwitter,
+//     faInstagram,
+//     faWhatsapp,
+//     faGithub,
+//     faLinkedinIn
+//   } from "@fortawesome/free-brands-svg-icons";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faYoutube,
-    faFacebook,
-    faTwitter,
-    faInstagram,
-    faWhatsapp,
-    faGithub,
-    faLinkedinIn
-  } from "@fortawesome/free-brands-svg-icons";
 
+import "./Footer.css";
 
-  import "./Footer.css"
-  
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <label className="logo-footer">Crypto </label>
+        <ul class="social-media-icons">
+          <li className="icons">
+            <a href="#">
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            </a>
+          </li>
+          <li className="icons">
+            <a href="#">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </li>
+          <li className="icons">
+            <a href="#">
+              <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+            </a>
+          </li>
 
-const Footer =()=>
-{
-    return(
-        <footer>
-            <div className="footer">
-              <label className="logo-footer">Crypto </label>
-              <ul class="social-media-icons">
-                  <li className="icons">
-                      <a href="#">
-                      <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-
-                      </a>
-                  </li>
-                  <li className="icons">
-                    <a href="#">
-                    <FontAwesomeIcon icon={faInstagram} size="2x" />
-                    </a>
-                  </li>
-                  <li className="icons">
-                      <a href="#">
-                      <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-                      </a>
-                  </li>
-
-                  
-                  <li className="icons">
-                      <a href="#">
-                      <FontAwesomeIcon icon={faTwitter} size="2x" />
-                      </a>
-                  </li>
-                  <li className="icons">
-                      <a href="#">
-                      <FontAwesomeIcon icon={faGithub} size="2x" />
-                      </a>
-                  </li>
-
-              </ul>
-              <div className="footer-bottom">
-                  <p>Copyright @2021 GBU.</p>
-            </div>
-            </div>
-        </footer>
-    )
-}
+          <li className="icons">
+            <a href="#">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </li>
+          <li className="icons">
+            <a href="#">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </li>
+        </ul>
+        <div className="footer-bottom">
+          <p>Copyright @2021 GBU.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 export default Footer;
