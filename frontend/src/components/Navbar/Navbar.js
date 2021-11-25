828974ee6a91b9c50dc9a2c312da39b5ef4eeef6
shivam <priyasaraswat766@gmail.com>
2021-11-25 18:30:56 +0530

    footer

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@
 import { Link,NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
+import Home from "../Home/Home";
 
 
 
@@ -71,7 +71,7 @@ function NavBar(props) {
   const NavMenuItems = [
     {
       id:"1",
-      url:"/Home",
+      url:"/",
       menu:"HOME",
       cname:"nav-menu"
     },
@@ -99,7 +99,8 @@ function NavBar(props) {
     <div>
       <nav className="nav-main">
         <label className="logo">
-          <NavLink to="/Home">Crypto </NavLink>
+         {/* <h>Crypto </h> */}
+         <NavLink to="/">Crypto</NavLink>
         </label>
          
         {/* <NavIcon/> */}
@@ -107,19 +108,19 @@ function NavBar(props) {
           {NavMenuItems.map(({ id, url, cname, menu }) => (
             
             <li>
-                
-                
-                <NavLink to={url} className={cname}>{menu}</NavLink>
-                
+                {/* <a href={url} className={cname}>{menu}</a> */}
+                 <NavLink to={url} className={cname}>{menu}</NavLink> 
+              
+                 
             </li>
           ))}
         </ul>
-        <div className="hamburger-menu">
+        {/* <div className="hamburger-menu">
           
           <FontAwesomeIcon icon={["fas", "coffee"]} />
           
 
-        </div>
+        </div> */}
       </nav>
     </div>
   );
