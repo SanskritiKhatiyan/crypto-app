1c1cffc1887f6ae670908192c93fc8b8015cd665
Abhishek <akthakuron120@gmail.com>
2021-11-25 19:07:56 +0530

    Revert "Revert "Revert "Changes"""
    This reverts commit 652aee6c3aa060c04f7b7da1b4a4b4edd0cbec0d.

--- frontend/src/components/FontAwsomeIcon/index.js
+++ frontend/src/components/FontAwsomeIcon/index.js
@@ -1,26 +1,4 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faNewspaper,
-  faCoins,
-  faEye,
-  faYoutube,
-  faFacebook,
-  faTwitter,
-  faInstagram,
-  faWhatsapp,
-  faGithub,
-  faLinkedinIn,
-} from "@fortawesome/free-solid-svg-icons";
+import { faNewspaper, faCoins, faEye } from "@fortawesome/free-solid-svg-icons";
 
-library.add(
-  faNewspaper,
-  faCoins,
-  faEye,
-  faYoutube,
-  faFacebook,
-  faTwitter,
-  faInstagram,
-  faWhatsapp,
-  faGithub,
-  faLinkedinIn
-);
+library.add(faNewspaper, faCoins, faEye);
