0d4cc552faeec770ba4f9c0205593d7d5e38cb02
Arjun Singh Rajput <rajputarjun1708@protonmail.com>
2021-11-26 23:30:48 +0530

    Price change styling and responsivness

--- frontend/src/components/Coins/CoinCard/coincard.js
+++ frontend/src/components/Coins/CoinCard/coincard.js
@@ -1,7 +1,6 @@
-
-
-
 import "./coincard.css";
+import up_arrow from "./img/up-arrow.png"
+import down_arrow from "./img/down-arrow.png"
 
 function watchlist(e) {
   e.target.setAttribute( 'src', 'https://img.icons8.com/ios-filled/20/000000/double-tick.png');
@@ -22,14 +21,16 @@ const App = (props) => {
       <div className="middle">
         <p>{props.currentPrice}</p>
         <p>{props.marketCap}</p>
-        
+        <p>Anything</p>
         {props.Percentage < 0 ? (
                 <p className="red">
                   {props.Percentage.toFixed(2)}%
+                  <img src={up_arrow} alt="increase arrow" /> 
                 </p>
               ) : (
                 <p className="green">
                   {props.Percentage.toFixed(2)}%
+                  <img src={down_arrow} alt="increase arrow" />
                 </p>
               )}
         
