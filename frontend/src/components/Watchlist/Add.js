cd43b67c40ae1d903c5a7bf14710f605d2ebf33d
Ashu <ashusengar2001@gmail.com>
2021-12-03 12:02:40 +0530

    Corrected Syntax Error

--- frontend/src/components/Watchlist/Add.js
+++ frontend/src/components/Watchlist/Add.js
@@ -1,17 +1,19 @@
-import React, {useState} from 'react';
-import Resultcard from "./Resultcard";
-
+import React, { useState } from "react";
+import Resultcard from "./resultcard";
 
 const Add = () => {
-    const [query, setQuery]= useState("");
-    const [results, setResults]= useState([]);
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
-    const onChange= (e)=> {e.preventDefault();
+  const onChange = (e) => {
+    e.preventDefault();
 
     setQuery(e.target.value);
 
-    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=16&page=1&sparkline=false&query=${e.target.value}`)
-    .then((res) => res.json())
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=16&page=1&sparkline=false&query=${e.target.value}`
+    )
+      .then((res) => res.json())
       .then((data) => {
         if (!data.errors) {
           setResults(data.results);
@@ -21,25 +23,25 @@ const Add = () => {
       });
   };
 
-    return (
-        <div>
-            <input
-              type="text"
-              placeholder="Search for a movie"
-              value={query}
-              onChange={onChange}
-            />
-            {results.length > 0 && (
-            <ul className="results">
-              {results.map((coin) => (
-                <li key={coin.id}>
-                  <Resultcard coin={coin} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-    )
-}
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for a movie"
+        value={query}
+        onChange={onChange}
+      />
+      {results.length > 0 && (
+        <ul className="results">
+          {results.map((coin) => (
+            <li key={coin.id}>
+              <Resultcard coin={coin} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Add;
