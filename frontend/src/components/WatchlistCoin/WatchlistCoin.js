59c2b91362f0ec27e6634f36e90d7836263271f1
Sanskriti Khatiyan <khatiyan.sanskriti@gmail.com>
2022-01-30 20:40:04 +0530

    Revert "Addded Icons on watchlist"
    This reverts commit ce4cf6fb3ec0c73985d50919bae458fb63f0b408.

--- frontend/src/components/WatchlistCoin/WatchlistCoin.js
+++ frontend/src/components/WatchlistCoin/WatchlistCoin.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./WatchlistCoin.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Notification from "../Notification";
+
 
 const WatchlistCoin = (props) => {
   const [notify, setnotify] = useState({
@@ -32,63 +33,48 @@ const WatchlistCoin = (props) => {
   };
 
   return (
-    <div className="watchlist-coin-container">
-      <div id="card">
-        <div className="top">
-          <p id="heading">{props.name}</p>
-          <div className="image-crop">
-            <img id="img_id" src={props.image} alt="coin-symbol" />
-          </div>
+    <div id="card">
+      <div className="top">
+        <p id="heading">{props.name}</p>
+        <div className="image-crop">
+          <img id="img_id" src={props.image} alt="" />
         </div>
-        <div className="middle">
-          <p>
-            Market Cap
-            <p className="mkt_cap">
-              <a>₹</a>
-              <a className="hidden">.</a>
-              {(props.marketCap / 1000000000).toFixed(0)} Cr
-            </p>
+      </div>
+      <div className="middle">
+        <p>
+          Market Cap
+          <p className="mkt_cap">
+            <a>₹</a>
+            <a className="hidden">.</a>
+            {(props.marketCap / 1000000000).toFixed(0)} Cr
           </p>
-          <p>
-            Price
-            <p className="price">
-              {" "}
-              <a>₹</a>
-              <a className="hidden">.</a> {props.currentPrice}
-            </p>
+        </p>
+        <p>
+          Price
+          <p className="price">
+            {" "}
+            <a>₹</a>
+            <a className="hidden">.</a> {props.currentPrice}
           </p>
-          {props.Percentage < 0 ? (
-            <p>
-              24h Change
-              <p className="red_card">
-                <img
-                  src="https://img.icons8.com/material/24/fa314a/give-way--v1.png"
-                  alt="hello"
-                  className="red-arrow"
-                />
-                {props.Percentage.toFixed(2)}%
-              </p>
-            </p>
-          ) : (
-            <p>
-              24h Change
-              <p className="green_card">
-                <img
-                  src="https://img.icons8.com/material/24/26e07f/sort-up--v2.png"
-                  alt="hello"
-                  className="green-arrow"
-                />
-                {props.Percentage.toFixed(2)}%
-              </p>
-            </p>
-          )}
-        </div>
-        {/* <Button></Button> */}
-        <div onClick={removeCoinFromWatchlist}>
-          <FontAwesomeIcon className="munusCircleIcon" icon={"minus-circle"} />
-        </div>
-        <Notification notify={notify} setnotify={setnotify} />
-      </div>{" "}
+        </p>
+        {props.Percentage < 0 ? (
+          <p>
+            24h Change
+            <p className="red_card">{props.Percentage.toFixed(2)}%</p>
+          </p>
+        ) : (
+          <p>
+            24h Change
+            <p className="green_card">{props.Percentage.toFixed(2)}%</p>
+          </p>
+        )}
+      </div>
+
+      {/* <Button></Button> */}
+      <div onClick={removeCoinFromWatchlist}>
+        <FontAwesomeIcon className="munusCircleIcon" icon={"minus-circle"} />
+      </div>
+      <Notification notify={notify} setnotify={setnotify} />
     </div>
   );
 };
