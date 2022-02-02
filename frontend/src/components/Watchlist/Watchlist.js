5f263bdee8459f891cae9b9e3782bde6f53064ed
Abhishek <akthakuron120@gmail.com>
2022-02-02 12:11:52 +0530

    Implemented render watchlist coin in better way.

--- frontend/src/components/Watchlist/Watchlist.js
+++ frontend/src/components/Watchlist/Watchlist.js
@@ -1,20 +1,19 @@
-import react, { useEffect, useState } from "react";
-import "./Watchlist.css";
+import React, { useState, useEffect } from "react";
 import WatchlistCoin from "../WatchlistCoin/WatchlistCoin";
-import WatchlistErrorPage from "../WatchlistErrorPage/WatchlistErrorPage";
-import { useHistory } from "react-router-dom";
+import "./Watchlist.css";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 import { CircularProgress } from "@material-ui/core";
 
-var coinsArrayData;
-
+var result;
 const Watchlist = () => {
-  const [data, setData] = useState([]);
+  const [coinData, setCoinData] = useState([]);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const history = useHistory();
 
-  useEffect(async () => {
+  const authenticateMiddleware = async () => {
     try {
       const response = await fetch("/watch-list", {
         method: "GET",
@@ -22,35 +21,15 @@ const Watchlist = () => {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
+        credentials: "include",
       });
 
-      const data = await response.json();
-      coinsArrayData = data.coinsArray;
-
-      if (coinsArrayData.length === 0) {
-        setLoading(false);
-      } else {
-        const newCoinArray = new Array();
-        for (let i = 0; i < coinsArrayData.length; i++) {
-          newCoinArray.push(coinsArrayData[i].coinName);
-        }
-        const stringArrayData = newCoinArray.toString();
-        stringArrayData.replace(/[\[\]']/g, "");
-        axios(
-          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=${stringArrayData}&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C200d%2C1y`
-        )
-          .then((response) => {
-            setData(response.data);
-          })
-          .catch((error) => {
-            console.error("Error fetching data: ", error);
-            setError(error);
-          })
-          .finally(() => {
-            setLoading(false);
-          });
+      const userResponse = await response.json();
+      const newCoinArray = new Array();
+      for (let i = 0; i < userResponse.coinsArray.length; i++) {
+        newCoinArray.push(userResponse.coinsArray[i].coinName);
       }
-
+      setCoinData(newCoinArray);
       if (!response.status === 200) {
         const error = new Error(response.error);
         throw error;
@@ -59,7 +38,29 @@ const Watchlist = () => {
       console.log(err);
       history.push("/signin");
     }
-  }, [coinsArrayData]);
+  };
+
+  const getAPIData = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    authenticateMiddleware();
+  }, []);
+
+  useEffect(() => {
+    getAPIData();
+  }, []);
 
   if (loading)
     return (
@@ -75,29 +76,25 @@ const Watchlist = () => {
       />
     );
 
-  if (error) return "Error...";
+  if (error) {
+    return <div>Error...</div>;
+  }
 
   return (
     <div className="card-align">
-      {data.length === 0 ? (
-        <div>
-          <WatchlistErrorPage />
-        </div>
-      ) : (
-        data.map((coins) => {
-          return (
-            <WatchlistCoin
-              ID={coins.id}
-              name={coins.name}
-              image={coins.image}
-              symbol={coins.symbol}
-              currentPrice={coins.current_price}
-              marketCap={coins.market_cap}
-              Percentage={coins.price_change_percentage_24h}
-            />
-          );
-        })
-      )}
+      {data
+        .filter((coin) => coinData.includes(coin.id))
+        .map((items) => (
+          <WatchlistCoin
+            ID={items.id}
+            name={items.name}
+            image={items.image}
+            symbol={items.symbol}
+            currentPrice={items.current_price}
+            marketCap={items.market_cap}
+            Percentage={items.price_change_percentage_24h}
+          />
+        ))}
     </div>
   );
 };
