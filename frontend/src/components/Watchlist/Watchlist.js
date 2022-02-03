76f5696e34ac7363b7f9c774ace8df5c98684a4f
Abhishek <akthakuron120@gmail.com>
2022-02-03 13:07:07 +0530

    API currency change

--- frontend/src/components/Watchlist/Watchlist.js
+++ frontend/src/components/Watchlist/Watchlist.js
@@ -42,7 +42,7 @@ const Watchlist = () => {
   const getAPIData = async () => {
     try {
       const response = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       );
       setData(response.data);
     } catch (error) {
